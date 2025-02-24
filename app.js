bb88c2aa00368e4ea5f43ed1175e4766d738dde9
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-02-24 18:10:39 +0530

    ..

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import express from "express";
 
 const app = express();
 
-const port = app.get("/", (req, res) => {
+const port = 3000;
+
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
